ac946fe4af27654b091578542d9e04ba4a7b8ffe
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-01-17 14:18:24 +0100

    CustomerManagement Project added

---
+++ CustomerManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TurboCollections;
+
+namespace CustomerManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turboList = new TurboList<float>();
+        }
+    }
+}
+
