608b2904de6e1f142e9ae560f74aa706a433908e
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-01-19 14:36:49 +0100

    Iturbostack extracted

---
+++ TurboCollections/ITurboStack.cs
@@ -0,0 +1,9 @@
+namespace TurboCollections;
+
+public interface ITurboStack<T>
+{
+    int Count { get; }
+    void Push(T item);
+    T Peek();
+    T Pop();
+}
--- TurboCollections/TurboStack.cs
+++ TurboCollections/TurboStack.cs
@@ -1,6 +1,7 @@
 namespace TurboCollections;
 
-public class TurboStack<T>
+
+public class TurboStack<T> : ITurboStack<T>
 {
     private T[] items = System.Array.Empty<T>();
     public int Count { get; private set; }
@@ -10,12 +11,10 @@ public class TurboStack<T>
         ArrayConfigurator.EnsureSize(ref items, Count+1);
         items[Count++] = item;
     }
-
     public T Peek()
     {
         return items[Count - 1];
     }
-
     public T Pop()
     {
         return items[--Count];
